9cccca5bfdc0ea717be06cf4346c084ea355fc41
Keyda <acaledesma@gmail.com>
2020-12-10 17:37:28 +0800

    classes

---
+++ assets/js/classes/PostmanRequest.js
@@ -0,0 +1,24 @@
+class PostmanRequest {
+
+    constructor(item) {
+        this.toBean(item);
+        console.log(this);
+
+    }
+
+    toBean(item) {
+        let { name } = item;
+        let { method, header, url } = item.request;
+        this.name = name;
+        this.method = method;
+        this.headers = header;
+        this.url = url;
+        this.fileName = this.generateFeatureFileName(name);
+    }
+
+    generateFeatureFileName(name) {
+        let mutated = name;
+        mutated = mutated.replaceAll(" ", "_");
+        return mutated + ".feature";
+    }
+}
--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,12 +1,15 @@
 $(document).ready(() => {
     let fileInput = $("#file-selector");
+    let features = $(".karate-content");
     $(fileInput).change(async function (event) {
         event.preventDefault();
         let fileList = event.currentTarget.files;
         let promise = promiseReadJSONFile(fileList[0]);
         let content = null;
         await promise.then((pm) => content = pm).catch((err) => console.log(err));
-        console.log(content)
+        content.item.forEach((item, index) => {
+            let postman = new PostmanRequest(item);
+        });
 
     });
 });
